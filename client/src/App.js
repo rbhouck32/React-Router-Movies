fe93e52c86de9113a3df96be109c3afa187ca129
Robert Houck <57123769+rbhouck32@users.noreply.github.com>
2020-02-12 02:35:54 -0500

    this is my attempt at completing the router movies section of the optional project.

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
-import React, { useState } from 'react';
-
-import SavedList from './Movies/SavedList';
-
+import React, { useState } from "react";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
+import SavedList from "./Movies/SavedList";
+import { Route } from "react-router-dom";
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+  const [savedList, setSavedList] = useState([]);
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
+    setSavedList([...savedList, movie]);
   };
 
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Route exact path="/" component={MovieList} />
+      <Route path="/movies/" component={Movie} />
     </div>
   );
 };
